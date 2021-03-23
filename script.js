da0bc26a753c3de0226b41fd7179cf502a39fc78
joanamdsantos <joanamlmsantos@gmail.com>
2021-03-23 12:36:25 +0000

    Add main  variables

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+var principal = "principal"
+var rate = "rate"
+var years = "years"
+var interest = principal * years * rate / 100
+
+// Logic to convert the 'No of Years' into the actual 
+// year in the future.
+
+var d = new Date()
+d.setFullYear(d.getFullYear() + years);
+document.getElementById("years").innerHTML = d;
+
+// function that reads the value of the range slider 
+// and displays it the <span>adjacent to the slider
+
+function 
+
+
 function compute()
 {
     p = document.getElementById("principal").value;
