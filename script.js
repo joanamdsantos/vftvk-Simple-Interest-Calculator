f4cd16bb5d1c32d5630097bca49e73c93d068a1d
joanamdsantos <joanamlmsantos@gmail.com>
2021-03-23 16:59:08 +0000

    feat(compute): Add compute function

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-var principal = "principal"
-var rate = "rate"
-var years = "years"
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
 var interest = principal * years * rate / 100
+var final = principal + 1 * interest
 
 // Logic to convert the 'No of Years' into the actual 
 // year in the future.
 
-var d = new Date()
-d.setFullYear(d.getFullYear() + years);
-document.getElementById("years").innerHTML = d;
+
 
 // function that reads the value of the range slider 
 // and displays it the <span>adjacent to the slider
@@ -18,10 +17,26 @@ function range() {
     document.getElementById('spanRate').innerHTML = output;   
 } 
 
-
 function compute()
 {
-    p = document.getElementById("principal").value;
+    p = document.getElementById("principal").value
+    r = document.getElementById("rate").value
+    y = document.getElementById("years").value
+    
+    principal = p
+    rate = r
+    years = y
+
+    var a_year = new Date()
+    a_year.setFullYear(a_year.getFullYear(year) + years);
+    document.getElementById("a_year").innerHTML = a_year;
+    
+    return principal, rate, year, interest, final, a_year
+    
+    document.getElementById("result").innerHTML = "If you deposit " + <span style='color:yellow;'> compute(principal) </span> + ",\
+    <br>at an interest rate of "<span style='color:yellow;'> compute(rate) </span> + ".\
+    <br>You will receive an amount of" + <span style='color:yellow;'> compute(final) </span> + ",\
+    <br>in the year" + <span style='color:yellow;'> compute(a_year) </span> + "."
+
     
 }
-        
