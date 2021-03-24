2f7b97e5fcb6fed2112032f50623a717ccfea706
joanamdsantos <joanamlmsantos@gmail.com>
2021-03-24 15:16:42 +0000

    feat(result): Add result computation message

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
 var principal = document.getElementById("principal").value;
 var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate / 100
+var years= document.getElementById("years").value;
+var interest = principal * year * rate / 100
 var final = principal + 1 * interest
-
-// Logic to convert the 'No of Years' into the actual 
-// year in the future.
-
 
 
 // function that reads the value of the range slider 
 // and displays it the <span>adjacent to the slider
-
 function range() {
-    output=document.getElementById("rate").value
-    document.getElementById('spanRate').innerHTML = output;   
-} 
+    r = document.getElementById("rate").value;
+    document.getElementById('spanRate').innerHTML = r + "%"; 
+}
 
-function compute()
-{
-    p = document.getElementById("principal").value
-    r = document.getElementById("rate").value
-    y = document.getElementById("years").value
+function compute(){
     
-    principal = p
-    rate = r
-    years = y
+    principal = document.getElementById("principal").value;
+    
+    // Get rate from range function
+    range();
+    rate = r;
 
-    var a_year = new Date()
-    a_year.setFullYear(a_year.getFullYear(year) + years);
-    document.getElementById("a_year").innerHTML = a_year;
-    
-    return principal, rate, year, interest, final, a_year
-    
-    document.getElementById("result").innerHTML = "If you deposit " + <span style='color:yellow;'> compute(principal) </span> + ",\
-    <br>at an interest rate of "<span style='color:yellow;'> compute(rate) </span> + ".\
-    <br>You will receive an amount of" + <span style='color:yellow;'> compute(final) </span> + ",\
-    <br>in the year" + <span style='color:yellow;'> compute(a_year) </span> + "."
+    // Get years from dropdown menu
+    var y = document.getElementById("years");
+    years = y.options[y.selectedIndex].value;
 
+    // Calculate interest
+    interest = principal * rate * years / 100;
     
+    // Logic to convert the 'No of Years' into the actual 
+    // year in the future.
+    const currentYear = new Date().getFullYear();
+    finalYear = (parseInt(currentYear) + parseInt(years) - 1);
+
+
+    // Write result message
+    var res = "<br>" + "If you deposit " + "<span class='highlight'>" +
+    principal + "</span>" + "," + "<br>" + "at an interest rate of "
+    + "<span class='highlight'>" + rate + "%" + "</span>" + "." + "<br>"
+    + "You will receive an amount of " + "<span class='highlight'>"
+    + interest + "</span>" + "," + "<br>"
+    + "in the year " + "<span class='highlight'>" + finalYear + "</span>"
+    + "." + "<br>";
+    
+    result = document.getElementById("result");
+    result.innerHTML = res;
+
 }
