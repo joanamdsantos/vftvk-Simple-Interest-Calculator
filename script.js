55d012f720f3a375098b2da1e858ff4c4fd76b6c
joanamdsantos <joanamlmsantos@gmail.com>
2021-03-26 19:24:52 +0000

    fix(principal box): fix principal box focus

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ function principal_validation() {
     pr = document.getElementById("principal").value;
     if (pr <= 0 || pr == "" || (alphaExp.test(pr) == true)) {
         alert("Enter a positive number");
-        document.getElementById('principal').onfocus();
-        document.getElementById('principal').blur();
+        setTimeout("document.getElementById('principal').focus();", 1);
         document.getElementById('principal').value = "";
         document.getElementById("result").innerHTML = "";
 
